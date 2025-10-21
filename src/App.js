2e1c2f6ee0aa7e8c34f5b8596b58240b51635f17
Asher Wayde <ashbwayde@gmail.com>
2025-10-21 12:29:04 -0400

    making the pages work?

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          It's React.js Time, Lets GOOOOOO!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
