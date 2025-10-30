d768a361c4ab5269d89649a57bc06ea1add46c52
Martin Ratchford <ratchfordm@etown.edu>
2025-10-30 13:11:02 -0400

    start of blog

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          It's React.js Time, Lets GOOOOOO!
-        </p>
+
+        
+          <title>
+            Etown Homelab Blog
+          </title>
+        
+
+        <div>
+          <h1>
+            Etown Homelab Blog
+          </h1>
+          <p>
+            Welcome to the Etown Homelab Blog! This is a space where we share insights, tutorials, and updates about our home lab projects and experiments.
+          </p>
+        </div>
+
+
+        <div>
+          <h2>
+            Posts
+          </h2>
+
+          <img src= "../images/meeting01.png" alt= "Meeting 01"/>
+          
+
+
+
+        </div>
+
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
